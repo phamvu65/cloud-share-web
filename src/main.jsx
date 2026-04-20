a7715f5635e0475a0750f63a59cfa926bb5feebb
= <=phamvu65cs@gmail.com>
2026-04-20 15:48:10 +0700

    add clerk login

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { ClerkProvider } from '@clerk/react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={clerkPubKey}>
     <App />
-  </StrictMode>,
+  </ClerkProvider>
 )
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,23 +7,31 @@ import CTASection from "../components/landing/CTASection";
 import Footer from "../components/landing/Footer";
 
 import { features, pricingPlans, testimonials } from "../assets/data.js";
+import { useClerk, useUser } from "@clerk/react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 const Landing = () => {
+  const { openSignIn, openSignUp } = useClerk();
+  const { isSignedIn } = useUser();
+  const navigate = useNavigate();
+
   return (
     <div className="landing-page bg-gradient-to-b from-gray-50 to-gray-100">
       {/* Hero Section */}
-      <HeroSection />
+      <HeroSection openSignIn={openSignIn} openSignUp={openSignUp} />
 
       {/* Features Section */}
       <FeaturesSection features={features} />
 
       {/* Pricing Section */}
-      <PricingSection pricingPlans={pricingPlans}  />
+      <PricingSection pricingPlans={pricingPlans} openSignUp={openSignUp} />
 
       {/* Testimonials Section */}
       <TestimonialsSection testimonials={testimonials} />
 
       {/* CTA Section */}
-      <CTASection  />
+      <CTASection openSignUp={openSignUp} />
 
       {/* Footer Section*/}
       <Footer />
