bb8572ec4661f01f4f7554cf452463ba115d2e75
= <=phamvu65cs@gmail.com>
2026-05-03 01:22:55 +0700

    feat(context): Create CreditsContext

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,50 @@ import Subscription from "./pages/Subscription.jsx";
 import Transactions from "./pages/Transactions.jsx";
 import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { Toaster } from "react-hot-toast";
+import { UserCreditsProvider } from "./context/UserCreditsContext.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Toaster  />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/dashboard" element={
-          <>
-            <SignedIn><Dashboard /></SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
-        <Route path="/upload" element={
-          <>
-            <SignedIn><Upload /></SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
-        <Route path="/my-files" element={
-          <>
-            <SignedIn><MyFiles /></SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
-        <Route path="/subscriptions" element={
-          <>
-            <SignedIn><Subscription /></SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
-        <Route path="/transactions" element={
-          <>
-            <SignedIn><Transactions /></SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
-        <Route path="/*" element={<RedirectToSignIn />} />
+    <UserCreditsProvider>
+      <BrowserRouter>
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={
+            <>
+              <SignedIn><Dashboard /></SignedIn>
+              <SignedOut><RedirectToSignIn /></SignedOut>
+            </>
+          } />
+          <Route path="/upload" element={
+            <>
+              <SignedIn><Upload /></SignedIn>
+              <SignedOut><RedirectToSignIn /></SignedOut>
+            </>
+          } />
+          <Route path="/my-files" element={
+            <>
+              <SignedIn><MyFiles /></SignedIn>
+              <SignedOut><RedirectToSignIn /></SignedOut>
+            </>
+          } />
+          <Route path="/subscriptions" element={
+            <>
+              <SignedIn><Subscription /></SignedIn>
+              <SignedOut><RedirectToSignIn /></SignedOut>
+            </>
+          } />
+          <Route path="/transactions" element={
+            <>
+              <SignedIn><Transactions /></SignedIn>
+              <SignedOut><RedirectToSignIn /></SignedOut>
+            </>
+          } />
+          <Route path="/*" element={<RedirectToSignIn />} />
 
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </UserCreditsProvider>
   )
 }
 
