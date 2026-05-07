6752789802f36c661b783dd841e4cd03ab5e75f8
= <=phamvu65cs@gmail.com>
2026-05-07 15:47:02 +0700

    chore: prepare frontend for production deploy

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api/v1.0";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1.0";
 
 export const apiEndpoints = {
     FETCH_FILES: `${BASE_URL}/files/my`,
